3f4082ce242577070f18705bf900d550228684c8
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2026-02-17 16:49:47 +0600

    Form Processing and validation

---
+++ FormCProcessing/FormCProcessing/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FormCProcessing.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Provide Username")]
+        [StringLength(10)]
+        public string Uname { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 8)]
+        public string Pass { get; set; }
+    }
+}
---
+++ FormCProcessing/FormCProcessing/Models/RegistrationModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FormCProcessing.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string[] Hobbies { get; set; }
+        [Range(1,40)]
+        public int Roll { get; set; }
+    }
+}
